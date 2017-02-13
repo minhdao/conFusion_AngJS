75d7d81d459f43a74ac91acda95a0d9cab765f8a
minhdao <minhdao6@gmail.com>
2017-02-13 18:11:10 +0700

    back out to when the code still works

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -77,25 +77,8 @@ angular.module('confusionApp', [])
 .controller('ContactController', ['$scope', function ($scope) {
     // feedback is accessable through FeedbackController too because of nested property of Angular
     $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:""};
-    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
-    $scope.channels = channels;
-    $scope.invalidChannelSelection = false;
 }])
 .controller('FeedbackController', ['$scope', function ($scope) {
-    $scope.sendFeedback = function(){
-        console.log($scope.feedback);
-        if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel){
-            $scope.invalidChannelSelection = true;
-            console.log('incorrect');
-        }else {
-            $scope.invalidChannelSelection = false;
-            $scope.feedback = {mychannel:"", firstName:"", lastName:"",
-            agree:false, email:"" };
-            $scope.feedback.mychannel="";
 
-            $scope.feedbackForm.$setPristine();
-            console.log($scope.feedback);
-        }
-    };
 }])
 ;
