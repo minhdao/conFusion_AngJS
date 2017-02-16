79a76fa8cf0ab2e3fa7ae18f82a38fb487e42383
minhdao <minhdao6@gmail.com>
2017-02-16 10:08:20 +0700

    change to customerName to author to push into comments array

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -147,16 +147,28 @@ angular.module('confusionApp', [])
 }])
 .controller('DishCommentController', ["$scope", function($scope){
     //Step 1: Create a JavaScript object to hold the comment from the form
-    $scope.customerComment = {customerName:"", rating:"5", comment:"", date:""};
+    $scope.customerComment = {rating:"5", comment:"", author:"", date:""};
     $scope.submitComment = function(){
         //Step 2: This is how you record the date
-            //"The date property of your JavaScript object holding the comment" = new Date().toISOString();
+        $scope.customerComment.date = new Date().toISOString();
+        console.log("name: " + $scope.customerComment.author);
+        console.log("rating: " + $scope.customerComment.rating);
+        console.log("comment: " + $scope.customerComment.comment);
+        console.log("date: " + $scope.customerComment.date);
+
         // Step 3: Push your comment into the dish's comment array
-            //$scope.dish.comments.push("Your JavaScript Object holding the comment");
+        $scope.dish.comments.push($scope.customerComment);
+        console.log("***** after pushing *****");
+        console.log("name: " + $scope.customerComment.author);
+        console.log("rating: " + $scope.customerComment.rating);
+        console.log("comment: " + $scope.customerComment.comment);
+        console.log("date: " + $scope.customerComment.date);
 
         //Step 4: reset your form to pristine
+        $scope.commentForm.$setPristine();
 
         //Step 5: reset your JavaScript object that holds your comment
+        $scope.customerComment = {rating:"5", comment:"", author:"", date:""};
     }
 }])
 ;
