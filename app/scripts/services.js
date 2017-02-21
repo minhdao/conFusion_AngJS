568df143c600c5d6ab94a7d093911df7df78eb92
minhdao <minhdao6@gmail.com>
2017-02-21 17:08:49 +0700

    fix build warnings and add media object to home page

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -202,7 +202,7 @@ angular.module('confusionApp')
 
                 this.getPromotion = function (index) {
                     return promotions[index];
-                }
+                };
 
 
         })
@@ -248,10 +248,10 @@ angular.module('confusionApp')
             // Remember this is a factory not a service
             corpfac.getLeaders = function () {
                 return leadership;
-            }
+            };
             corpfac.getLeader = function (index) {
                 return leadership[index];
-            }
+            };
             return corpfac;
 
         })
