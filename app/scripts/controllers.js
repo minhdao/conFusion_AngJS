17f51cb1faa892bf9a928b0445b9b2553069f0dd
minhdao <minhdao6@gmail.com>
2017-02-28 20:51:58 +0700

    add code to save data to to server

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -90,16 +90,20 @@ angular.module('confusionApp')
     );
 }])
 
-.controller('DishCommentController', ['$scope', function($scope) {
+.controller('DishCommentController', ['$scope','menuFactory', function($scope, menuFactory) {
 
     $scope.mycomment = {rating:5, comment:"", author:"", date:""};
 
     $scope.submitComment = function () {
 
         $scope.mycomment.date = new Date().toISOString();
+        
         console.log($scope.mycomment);
 
         $scope.dish.comments.push($scope.mycomment);
+
+        // submit comments to server
+        menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
 
         $scope.commentForm.$setPristine();
 
